e7145acfadaf4b780700aa293e7bd886bbfbe4c8
vysogota <moriarty1904@mail.ru>
2025-10-04 02:52:59 +0300

    try/catch

--- cpp-training/lesson_02/vector/main.cpp
+++ cpp-training/lesson_02/vector/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cstddef>
 #include <initializer_list>
 #include <iostream>
@@ -11,37 +12,70 @@ public:
     Vector (std::initializer_list<T> list) : size_(list.size()), capacity_(list.size())
     {
         ptr = (T*)::operator new(sizeof(T)*capacity_);
-        for (int i{}; i < capacity_; ++i)
-        {
-            new(ptr+i) T(*(list.begin()+i));
+        int i{};
+        try {
+            for (; i < capacity_; ++i)
+            {
+                new(ptr+i) T(*(list.begin()+i));
+            }
+        } catch (...) {
+            for (int j{}; j < i; ++j)
+                ptr[j].~T();
+            operator delete(ptr);
+            throw;
         }
     }
 
     Vector (int size) : size_(size), capacity_(size)
     {
         ptr = (T*)operator new(sizeof(T)*capacity_);
-        for (int i{}; i < size; ++i)
-            new(ptr+i) T();
+        int i{};
+        try {
+            for (; i < capacity_; ++i)
+               new(ptr+i) T();
+        } catch (...) {
+           for (int j{}; j < i; ++j)
+                ptr[j].~T();
+            operator delete(ptr);
+            throw;
+        }
     }
 
     Vector (int size, T value) : size_(size), capacity_(size)
     {
         ptr = (T*)operator new(sizeof(T)*capacity_);
-        for (int i{}; i < size; ++i)
-            new(ptr+i) T(value);
+        int i{};
+        try {
+            for (int i{}; i < capacity_; ++i)
+                new(ptr+i) T(value);
+        } catch (...) {
+            for (int j{}; j < i; ++j)
+                ptr[j].~T();
+            operator delete(ptr);
+            throw;
+        }
     }
 
     Vector (const Vector& other) : size_(other.size_), capacity_(other.capacity_)
     {
         ptr = (T*)operator new(sizeof(T)*other.capacity_);
-        for (int i{}; i < size_; ++i)
-            new(ptr+i) T(other.ptr[i]);
+        int i{};
+        try {
+            for (; i < size_; ++i)
+                new(ptr+i) T(other.ptr[i]);
+        } catch (...) {
+            for (int j{}; j < i; ++j)
+                ptr[j].~T();
+            operator delete(ptr);
+            throw;
+        }
     }
 
-    Vector (Vector&& other) : size_(other.size_), capacity_(other.capacity_)
+    Vector (Vector&& other) noexcept : size_(other.size_), capacity_(other.capacity_), ptr(other.ptr)
     {
-        ptr = other.ptr;
         other.ptr = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
     }
 
     void print()
@@ -71,19 +105,27 @@ public:
         operator delete(ptr);
     }
 
-    size_t size () { return size_; }
-    size_t capacity() { return capacity_; }
+    size_t size () const { return size_; }
+    size_t capacity() const { return capacity_; }
 
 private:
     void vecrotFull()
     {
         capacity_*=2;
         T* temp = (T*)operator new(sizeof(T)*capacity_);
-        for (int i{}; i < size_; ++i)
-        {
-            new(temp+i) T(ptr[i]);
-            //if (i < size_) new(temp+i) T(ptr[i]);
-            //else new(temp+i) T();
+        int i{};
+        try {
+            for (; i < size_; ++i)
+            {
+                new(temp+i) T(ptr[i]);
+                //if (i < size_) new(temp+i) T(ptr[i]);
+                //else new(temp+i) T();
+            }
+        } catch (...) {
+            for (int j{}; j < i; ++j)
+                temp[j].~T();
+            operator delete(temp);
+            throw;
         }
         std::swap(temp, ptr);
         for (int i{}; i < size_; ++i)
@@ -95,6 +137,98 @@ private:
     size_t capacity_{};
 };
 
+void test_push_back()
+{
+    Vector<int> vec(2);
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+
+    assert(vec.size() == 5);
+    assert(vec.capacity() == 8);
+    assert(vec[2] == 10);
+    assert(vec[3] == 20);
+    assert(vec[4] == 30);
+}
+
+void test_constreuctor()
+{
+    Vector<int> vec(3, 84);
+    assert(vec.size() == 3);
+    assert(vec.capacity() == 3);
+    assert(vec[0] == 84);
+    assert(vec[1] == 84);
+    assert(vec[2] == 84);
+}
+
+void test_initializer_list()
+{
+    Vector<int> vec {1,2,3,4,5};
+    assert(vec.size() == 5);
+    assert(vec.capacity() == 5);
+
+    for (int i{}; i < 5; ++i)
+        assert(vec[i] == i+1);
+}
+
+void test_copy_constructor()
+{
+    Vector<int> vec {1,2,3};
+    Vector<int> copy(vec);
+
+    assert(copy.size() == 3);
+    assert(copy.capacity() == 3);
+
+    for (int i{}; i < 3; ++i)
+    {
+        assert(vec[i] == i+1);
+        assert(copy[i] == i+1);
+    }
+
+    copy[0] = 5;
+    assert(copy[0] == 5);
+    assert(vec[0] == 1);
+}
+
+void test_move_constructor()
+{
+    Vector<int> vec {1,2,3};
+    Vector<int> move (std::move(vec));
+
+    assert(move[0] == 1);
+    assert(move[1] == 2);
+    assert(move[2] == 3);
+    
+    assert(vec.size() == 0 && vec.capacity() == 0);
+}
+
+void test()
+{
+    class TestClass
+    {
+    public:
+        TestClass() {
+            throw "Copy ERROR";
+        }
+    };
+
+
+    try {
+        Vector<TestClass> vec(5);
+    } catch (const char* e) {
+        std::cout << e << std::endl;
+        assert(std::string(e) == "Copy ERROR");
+    }
+}
+
 int main()
 {
+    test_constreuctor();
+    test_copy_constructor();
+    test_initializer_list();
+    test_push_back();
+    test_move_constructor();
+    test();
+
+    std::cout << "All tests passed" << std::endl;
 }
